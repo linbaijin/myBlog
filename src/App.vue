fbf699507f96a10d2ce98df55932deaeafda8d03
LBJ <576117134@qq.com>
2019-03-24 22:43:38 +0800

    继续页面布局

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
             <mu-button flat large color="warning">JavaScript</mu-button>
             </div>
           </mu-col>
-        </mu-row>
+          </mu-row>
         </div>
         
         <!-- 导航栏结束 -->
@@ -54,13 +54,77 @@
           </div>
           
         </mu-row>
-   
+        <!-- 龙猫结束 -->
+
+        <!-- 下方文章主体 -->
+        <div id="home">
+          <div id="main">
+            <div id="main-content">
+              <mu-row>
+                <!-- 下方左侧 -->
+                <mu-col sm="2" md="2" lg="2">
+                  <div id="left-main">
+                    <div class="personal-info">
+                      <div class="head-pic">
+                        <img id="head-pic" src="./assets/headpic.jpg" class="img">
+                      </div>
+                      
+                      <div class="text-box">
+                        那一天的河川
+                      宛若封存了千年的故事
+                      不由自主的穿过了幽幽的隧道
+                      忽然的瞬间在那久弃的教堂
+                      我看见阳光亘古不变透过彩璃窗
+                      蓝天 白云 青草 旧屋 微风
+                      一切都静逸不变仿佛时光静止
+                      </div>                      
+                    </div>
+
+                    <div id="subtitle">
+                      <div id="subtitlelist" class="sub-list">
+                        <ul>
+                          <li><p>Linux</p></li>
+                          <li><p>Java</p></li>
+                          <li><p>C++</p></li>
+                          <li><p>Python</p></li>
+                          <li><p>JavaScript</p></li>
+                          <li><p>RegExp正则</p></li>
+                          <li><p>Vue</p></li>
+                          <li><p>React</p></li>
+                          <li><p>Node</p></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </mu-col>
+                <!-- 下方右侧 -->
+                <mu-col offset="1" sm="9" md="9" lg="9">
+                  <div id="right-main">
+                    <div id="article-area">
+                      <blogArticle v-for="(item,index) in list" :key="index"></blogArticle>
+                    </div>
+                  </div>
+                </mu-col>
+              </mu-row>
+            </div>
+          </div>
+        </div>
+
 </div>
 </template>
 
 <script>
+import blogArticle from '@/components/component/blogArticle'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      list:[1,2,3,4,5,6,7,8]
+    }
+  },
+  components:{
+    blogArticle
+  }
 }
 </script>
 
@@ -144,5 +208,104 @@ h2 figure img {
   animation-iteration-count: 1,infinite;
   animation-fill-mode: forwards;
   animation-direction: normal,alternate;
+}
+#home{
+  margin: 0 auto;
+}
+#main{
+  overflow: hidden;
+  padding: 20px 6% 10px 6%;
+  position: relative;
+}
+#main-content{
+  width: 100%;
+  position: relative;
+}
+#right-main #left-main{
+  width: 100%;
+  height: auto;
+}
+.personal-info{
+  width: 100%;
+  height: 30%;
+  background: none repeat scroll 0% 0% #fafcfd;
+  border-radius: 7px;
+  box-shadow: 1px 1px 2px #a7a8ad;
+  line-height: 23px;
+  margin-bottom: 15px;
+  padding: 10px;
+  text-indent: 0px;
+}
+#head-pic{
+  transition: all .6s;
+}
+.text-box{
+  width: 100%;
+  overflow: hidden;
+  color: #46241b;
+  text-overflow: ellipsis;
+  white-space:normal;
+}
+.img{
+  width: 100%;
+  border-radius: 50%;
+  
+}
+.head-pic{
+  width: 100%;
+}
+#subtitle{
+  width: 100%;
+  height: auto;
+}
+.sub-list{
+  width: 100%;
+  height: auto;
+  background: none repeat scroll 0% 0% #fafcfd;
+  border-radius: 7px;
+  box-shadow: 1px 1px 2px #a7a8ad;
+  margin-bottom: 15px;
+  overflow: hidden;
+}
+p{
+  padding: 0;
+  margin: 0;
+}
+.sub-list ul li{
+  font-family: Microsoft YaHei, arial, serif;
+  font-size: 20px;
+  height: 18%;
+  width: 100%;
+  list-style: none;
+  border-bottom: 1px solid #dadfe1;
+  padding: 10px 5px;
+  text-indent: 1em;
+  padding: 10px 5px;
+}
+.sub-list ul li:last-child{
+  border-bottom: 0;
+}
+#subtitlelist ul{
+  width: 100%;
+  padding: 0;
+  line-height: 1.5;
+}
+#subtitlelist ul li p:hover{
+  width: 100%;
+  height: auto;
+  animation: subtitle_li .7s ease-in-out .5s infinite normal
+}
+@keyframes subtitle_li{
+  0%{
+    transform: scale(0.9);
+  }
+  100%{
+    transform: scale(1);
+  }
+}
+
+#article-area{
+  width: 100%;
+  height: auto;
 }
 </style>
---
+++ src/components/component/blogArticle.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="article">
+        <div class="article-title">
+            <a href="#">玩的卡号的开始</a>
+        </div>
+        <div class="abstract">
+            <div class="abstract-desc">
+                东方嘉盛防守打法被害机申达股份史蒂芬
+                的几哈放假后不久就会不会技术部副放声大哭
+                是的恢复不上班方式基本符合结不结婚水电费是否是
+                东方嘉盛防守打法被害机申达股份史蒂芬
+                的几哈放假后不久就会不会技术部副放声大哭
+                是的恢复不上班方式基本符合结不结婚水电费是否是
+                东方嘉盛防守打法被害机申达股份史蒂芬
+                的几哈放假后不久就会不会技术部副放声大哭
+                是的恢复不上班方式基本符合结不结婚水电费是否是
+            </div>
+            <div class="abstract-foot">
+                 2018-12-10 00:47 Model-Zachary 阅读(51) 评论(0) 编辑
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.article{
+    background: none repeat scroll 0% 0% #ffffff;
+    border-radius: 7px;
+    box-shadow: 1px 2px 3px #a7a8ad;
+    color: #333331;
+    margin:0px 5px 18px 0px;
+    min-height: 10px;
+    padding: 5px 20px 10px;
+    animation: pullUp 2s linear 0.5s 1 normal
+}
+@keyframes pullUp{
+    0%{
+        transform: translateX(300px);
+        opacity: 0;
+    }
+    50%{
+        transform: translateX(150px);
+        opacity: 0.5;
+    }
+    100%{
+        transform: translateX(0);
+        opacity: 1;
+    }
+}
+.article-title{
+    border-bottom: 1px dashed #4ab19a;
+    clear: both;
+    width: 100%;
+    line-height: 2.5em;
+    font-size: 21px;
+}
+.abstract{
+    padding: 10px 0px 4px;
+    width: 100%
+}
+.abstract-desc{
+    word-wrap: break-word;
+    word-break: break-all;
+    overflow: hidden;
+    line-height: 1.5
+}
+.abstract-foot{
+    clear: both;
+    width: 100%;
+    text-align: right;
+    overflow: hidden;
+    white-space: nowrap;
+}
+</style>
